11c0b8d1bf0fba18a169a3945a1ea3f2cba479e1
Lukas Kostal <93650268+KostalLukas@users.noreply.github.com>
2023-08-21 09:51:32 +0200

    Update Diode Comparison Analysis.py

--- Diode Test/Diode Comparison Analysis.py
+++ Diode Test/Diode Comparison Analysis.py
@@ -10,6 +10,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+# function to print to console and file simultaneously
+def tprint(text=''):
+    print(text)
+    global file
+    with open(file, 'a') as output:
+        print(text, file=output)
+
+
 # list of photodiodes to be tested
 pds = ['D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8']
 
@@ -56,15 +64,16 @@ for i in range(0, len (pds)):
 
     # if test lasted more than 150s crop it at 110s
     if n > 150:
-        V = V[0: 110]
-        n = 110
+        V = V[0: 105]
+        n = 105
 
     # array of sampling times
     t = np.arange(n) / fs
 
-    # calculate mean and standard deviation of the measured voltage
+    # calculate mean, standard deviation and standard error of measured voltage
     V_avg[i] = np.mean(V)
     V_std[i] = np.std(V)
+    V_sem[i] = V_std[i] / np.sqrt(len(V))
 
     # check if should be plotted and plot
     if pds[i] in plot:
@@ -74,8 +83,16 @@ if len(plot) > 0:
     plt.legend(loc=(-0.2, 1.05), ncol=len(plot))
     plt.savefig('Output/Comparison_voltages.png', dpi=300, bbox_inches='tight')
 
-# calculate standard error on the mean for measured votlage
-V_sem = np.std(V)
+P_sem = V_sem * P / V_avg
+
+# print the numerical results
+file = 'Output/Comparison_results.csv'
+open(file, 'w')
+
+tprint('diode, mean voltage (V), standard deviation (V), standard error (V)')
+
+for i in range(0, len(pds)):
+    tprint(f'{pds[i]}, {V_avg[i]:.6g}, {V_std[i]:.4g}, {V_sem[i]:.4g}')
 
 # parameters for plotting mean measured voltage for each photodiode
 plt.figure(2)
@@ -87,6 +104,17 @@ plt.grid()
 
 plt.errorbar(pds, V_avg, yerr=V_sem, marker='x', ls='none', capsize=5, c=colr[0])
 plt.savefig('Output/Comparison_mean.png', dpi=300, bbox_inches='tight')
+
+# parameters for plotting SEM of power for each photodiode
+plt.figure(3)
+plt.title('SEM of Calculated Power')
+plt.xlabel('photodiode')
+plt.ylabel('SEM $\sigma_{P}$ ($\mu W$)')
+plt.rc('grid', linestyle=':', c='black', alpha=0.8)
+plt.grid()
+
+plt.plot(pds, P_sem, marker='x', ls='none', c=colr[0])
+plt.savefig('Output/Comparison_sem.png', dpi=300, bbox_inches='tight')
 plt.show()
 
 # show the plots
