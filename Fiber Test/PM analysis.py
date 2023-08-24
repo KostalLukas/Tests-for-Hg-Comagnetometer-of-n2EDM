8e7f7dba6ab4f152653dad9ff8ea1d65821dba11
Lukas Kostal <93650268+KostalLukas@users.noreply.github.com>
2023-08-24 17:43:15 +0200

    Add files via upload

--- Fiber Test/PM analysis.py
+++ Fiber Test/PM analysis.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Fiber Test Power Meter Analysis v2.0
+Fiber Test Power Meter Analysis v3.0
 
-Lukas Kostal, 26.7.2023, PSI
+Lukas Kostal, 24.8.2023, PSI
 """
 
 
@@ -16,22 +16,13 @@ def tprint(text=''):
     global file
     with open(file, 'a') as output:
         print(text, file=output)
-
-
-# function to calcualte mean and standard error on the mean
-def get_mean(arr):
-    n = len(arr) - np.sum(np.isnan(arr))
-    mean = np.nansum(arr) / n
-    sem = np.sqrt(np.nansum( (arr - mean)**2 )) / n
-    
-    return(mean, sem)
     
 
 # relative error in power meter measurements
 err = 0.05
 
 # specify colors for plotting
-clr = ['blue', 'limegreen', 'orange']
+colr = ['blue', 'green', 'orange', 'red']
 
 # load the data
 data = np.loadtxt('Data/Powermeter_data.csv', unpack=True, delimiter=',', skiprows=1)
@@ -57,7 +48,7 @@ for i in range(0, n):
 # convert power to mW
 data[4:9, :] /= 1000
 
-# slice the data into measurements P in uW and ADC in V
+# slice the data into measurements P in uW and DAQ in V
 Ptot = data[4, :] 
 Pbst = data[5, :]
 Pbsr = data[6, :]
@@ -74,38 +65,87 @@ Pfi_err = Pfi * err
 Pfo_err = Pfo * err
 
 # transmission from power meter measurement with beam splitter
-T_pwmb = Pfo / Pbsr / 2
-T_pwmb_err = T_pwmb * np.sqrt(2 * err**2)
+T_pmbs = Pfo / Pbsr / 2
+T_pmbs_err = T_pmbs * np.sqrt(2 * err**2)
+
+T_pmbs_avg = np.nanmean(T_pmbs)
+T_pmbs_avgerr = np.sqrt(np.nansum(T_pmbs_err**2)) / np.sqrt(np.sum(~np.isnan(T_pmbs_err)))
+T_pmbs_ptp = np.ptp(T_pmbs[~np.isnan(T_pmbs)])
 
 # transmission from power meter measurement fiber only
-T_pwmf = Pfo / Pfi
-T_pwmf_err = T_pwmf * np.sqrt(2 * err**2)
+T_pmf = Pfo / Pfi
+T_pmf_err = T_pmf * np.sqrt(2 * err**2)
 
-# find average calibration for ADC
-cal1_mean, cal1_sem = get_mean(Pbsr / Ch1)
-cal2_mean, cal2_sem = get_mean(Pfo / Ch2)
+T_pmf_avg = np.nanmean(T_pmf)
+T_pmf_avgerr = np.sqrt(np.nansum(T_pmf_err**2)) / np.sqrt(np.sum(~np.isnan(T_pmf_err)))
+T_pmf_ptp = np.ptp(T_pmf[~np.isnan(T_pmf)])
 
-# tranmission from ADC
-T_adc = (Ch2 * cal2_mean) / (Ch1 * cal1_mean) / 2
-T_adc_err = T_adc * np.sqrt((cal2_sem / cal2_mean)**2 + (cal1_sem / cal1_mean)**2)
+cal1_arr = Pbsr / Ch1
+cal2_arr = Pfo / Ch2
+
+cal1_err = cal1_arr * err
+cal2_err = cal2_arr * err
+
+cal1_avg = np.nanmean(cal1_arr)
+cal2_avg = np.nanmean(cal2_arr)
+
+cal1_avgerr = np.sqrt(np.nansum(cal1_err**2)) / np.sqrt(np.sum(~np.isnan(cal1_err)))
+cal2_avgerr = np.sqrt(np.nansum(cal2_err**2)) / np.sqrt(np.sum(~np.isnan(cal2_err)))
+
+cal1_ptp = np.ptp(cal1_arr[~np.isnan(cal1_arr)])
+cal2_ptp = np.ptp(cal2_arr[~np.isnan(cal2_arr)])
+
+# tranmission from DAQ
+T_daq = (Ch2 * cal2_avg) / (Ch1 * cal1_avg) / 2
+T_daq_err = T_daq * np.sqrt((cal2_avgerr / cal2_avg)**2 + (cal1_avgerr / cal1_avg)**2)
+
+T_daq_avg = np.nanmean(T_daq)
+T_daq_avgerr = np.sqrt(np.nansum(T_daq_err**2)) / np.sqrt(np.sum(~np.isnan(T_daq_err)))
+T_daq_ptp = np.ptp(T_daq[~np.isnan(T_daq)])
 
 # beamsplitter ratio of reflected over transmitted
-R_bs = Pbsr / Pbst
-R_bs_err = R_bs * np.sqrt(2 * err**2)
+R = Pbsr / Pbst
+R_err = R * np.sqrt(2 * err**2)
 
-# ADC calibration constants
-cal1 = Pbsr / Ch1
-cal1_err = cal1 * err
-cal2 = Pfo / Ch2
-cal2_err = cal2 * err
+R_avg = np.nanmean(R)
+R_avgerr = np.sqrt(np.nansum(R_err**2)) / np.sqrt(np.sum(~np.isnan(R_err)))
+R_ptp = np.ptp(R[~np.isnan(R)])
 
 # print the numerical results
 file = 'Output/PM_results.txt'
 open(file, 'w')
 
-tprint(f'Ch1 average calibration constant    = {cal1_mean:.4g} ± {cal1_sem:.2g} mW V^-1')
-tprint(f'Ch2 average calibration constant    = {cal2_mean:.4g} ± {cal2_sem:.2g} mW V^-1')
-
+tprint('Fiber Transmission:')
+tprint('PM measurement fiber only:')
+tprint(f'T_avg = {T_pmf_avg:.4g}')
+tprint(f'T_err = {T_pmf_avg:.4g}')
+tprint(f'T_ptp = {T_pmf_avg:.4g}')
+tprint('PM measurement:')
+tprint(f'T_avg = {T_pmbs_avg:.4g}')
+tprint(f'T_err = {T_pmbs_avg:.4g}')
+tprint(f'T_ptp = {T_pmbs_avg:.4g}')
+tprint('DAQ measurement:')
+tprint(f'T_avg = {T_daq_avg:.4g}')
+tprint(f'T_err = {T_daq_avg:.4g}')
+tprint(f'T_ptp = {T_daq_avg:.4g}')
+tprint()
+tprint('Beamsplitter ratio:')
+tprint('PM measurement:')
+tprint(f'R_avg = {R_avg:.4g}')
+tprint(f'R_err = {R_avg:.4g}')
+tprint(f'R_ptp = {R_avg:.4g}')
+tprint('DAQ measurement:')
+tprint(f'R_avg = {R_avg:.4g}')
+tprint(f'R_err = {R_avg:.4g}')
+tprint(f'R_ptp = {R_avg:.4g}')
+tprint()
+tprint('Calibration constants:')
+tprint(f'Ch1_avg = {cal1_avg:.4g} uW')
+tprint(f'Ch1_err = {cal1_avgerr:.4g} uW')
+tprint(f'Ch1_ptp = {cal1_ptp:.4g} uW')
+tprint(f'Ch2_avg = {cal2_avg:.4g} uW')
+tprint(f'Ch2_err = {cal2_avgerr:.4g} uW')
+tprint(f'Ch2_ptp = {cal2_ptp:.4g} uW')
 
 # parameters for plotting fiber transmission
 plt.figure(1)
@@ -116,9 +156,9 @@ plt.rc('grid', linestyle=':', c='black', alpha=0.8)
 plt.grid()
 
 # plot the transmission ratios
-plt.errorbar(t, T_pwmb, yerr=T_pwmb_err, fmt='.', capsize=5, c=clr[0], label='$T_{power \; meter}$')
-plt.errorbar(t, T_pwmf, yerr=T_pwmf_err, fmt='.', capsize=5, c=clr[1], label='$T_{fiber \; only}$')
-plt.errorbar(t, T_adc, yerr=T_adc_err, fmt='.', capsize=5, c=clr[2], label='$T_{ADC}$')
+plt.errorbar(t, T_pmbs, yerr=T_pmbs_err, fmt='.', capsize=5, c=colr[0], label='$T_{power \; meter}$')
+plt.errorbar(t, T_pmf, yerr=T_pmf_err, fmt='.', capsize=5, c=colr[1], label='$T_{fiber \; only}$')
+plt.errorbar(t, T_daq, yerr=T_daq_err, fmt='.', capsize=5, c=colr[2], label='$T_{DAQ}$')
 
 plt.legend(loc=(0.1, 1.05), ncol=3)
 
@@ -134,7 +174,7 @@ plt.rc('grid', linestyle=':', c='black', alpha=0.8)
 plt.grid()
 
 # plot the beamsplitter ratio
-plt.errorbar(t, R_bs, yerr=R_bs_err, fmt='.', capsize=5, c=clr[0])
+plt.errorbar(t, R, yerr=R_err, fmt='.', capsize=5, c=colr[0])
 
 # save plot
 plt.savefig('Output/PM_beamsplitter.png', dpi=300, bbox_inches='tight')
@@ -149,7 +189,7 @@ plt.rc('grid', linestyle=':', c='black', alpha=0.8)
 plt.grid()
 
 # plot the total laser output power
-plt.errorbar(t, Ptot, yerr=Ptot_err, fmt='.', capsize=5, c=clr[0])
+plt.errorbar(t, Ptot, yerr=Ptot_err, fmt='.', capsize=5, c=colr[0])
 
 # save the plot
 plt.savefig('Output/PM_power.png', dpi=300, bbox_inches='tight')
@@ -158,13 +198,13 @@ plt.savefig('Output/PM_power.png', dpi=300, bbox_inches='tight')
 plt.figure(4)
 plt.title('Photodiode Degradation over Time', pad=40)
 plt.xlabel('time $t$ (h)')
-plt.ylabel('$P_{ADC} / P_{power \; meter}$ (unitless)')
+plt.ylabel('$P_{DAQ} / P_{power \; meter}$ (unitless)')
 plt.rc('grid', linestyle=':', c='black', alpha=0.8)
 plt.grid()
 
-# plot the ADC calibration constants
-plt.errorbar(t, cal1, yerr=cal1_err, fmt='.', capsize=5, c=clr[0], label='ADC Ch1 (beamsplitter reflection)')
-plt.errorbar(t, cal2, yerr=cal2_err, fmt='.', capsize=5, c=clr[2], label='ADC Ch2 (fiber output)')
+# plot the daq calibration constants
+plt.errorbar(t, cal1_arr, yerr=cal1_err, fmt='.', capsize=5, c=colr[0], label='DAQ Ch1 (beamsplitter reflection)')
+plt.errorbar(t, cal2_arr, yerr=cal2_err, fmt='.', capsize=5, c=colr[3], label='DAQ Ch2 (fiber output)')
 
 plt.legend(loc=(-0.1, 1.05), ncol=2)
 
