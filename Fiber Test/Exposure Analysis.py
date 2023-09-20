a90628ec1b241046caa841e17670242098664ed1
Lukas Kostal <93650268+KostalLukas@users.noreply.github.com>
2023-09-20 12:40:58 +0200

    Add files via upload

--- Fiber Test/Exposure Analysis.py
+++ Fiber Test/Exposure Analysis.py
@@ -12,13 +12,35 @@ import glob as gb
 import sys
 
 
-# function to read arguments from console
-def garg(*args):
-    args = list(args)
-    arg_sys = sys.argv
-    for i in range(1, len(arg_sys)):
-        args[i-1] = type(args[i-1])(arg_sys[i])
-    return args
+# function to pass arguments from console
+def parg(*arg_var):
+    arg_sys = sys.argv[1:]
+    
+    arg_name = []
+    arg_type = []
+    for i in range(0, len(arg_var)):
+        arg_id = id(arg_var[i])
+        
+        for key in globals().keys():
+            if key[0] != '_':
+                val = globals()[key]
+                if id(val) == arg_id:
+                    arg_name.append(key)
+                    arg_type.append(type(val))
+                
+    for i in range(0, len(arg_sys)):
+        for j in range(0, len(arg_var)):
+            if arg_sys[i].split('=')[0] == arg_name[j]:
+                
+                arg_val = arg_sys[i].split('=')[1]
+                
+                if arg_val == 'm':
+                    arg_val = 1/60
+                if arg_val == 'h':
+                    arg_val = 1/3600
+                
+                globals()[arg_name[j]] = arg_type[j](arg_val)
+    return None
 
     
 # function to print to console and file simultaneously
@@ -39,7 +61,8 @@ fs  = 10
 # taking 2024 to be a leap year otherwise change 29 -> 28
 mtd = np.array([31, 30, 29, 31, 30, 31, 30, 31, 30, 31, 30, 31])
 
-dt_th = garg(dt_th)[0]
+# get threshold for time difference
+parg(dt_th)
 
 # get a sorted list of all datasets in the Data directory
 ds = gb.glob('Data/UV_new10m_**')
